79dab2ee269c383f811b22440bc8a6b042d2d65c
sndmnn <hot.zoo@hotmail.com>
2020-03-27 21:07:56 -0300

    Validation added to the routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const { celebrate, Segments, Joi } = require('celebrate')
+
 const NgoController = require('./controller/NgoController')
 const IncidentController = require('./controller/IncidentController')
 const ProfileController = require('./controller/ProfileController')
@@ -6,16 +8,53 @@ const SessionController = require('./controller/SessionController')
 
 const routes = express.Router()
 
-routes.post('/sessions', SessionController.create)
+routes.post('/sessions', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    ngoId: Joi.string().required()
+  })
+}),SessionController.create)
 
 routes.get('/ngos', NgoController.index)
-routes.post('/ngos', NgoController.create)
 
-routes.get('/profile', ProfileController.index)
+routes.post('/ngos', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
+    whatsapp: Joi.number().required().min(10).max(11),
+    city: Joi.string().required(),
+    uf: Joi.string().required().length(2)
+  })
+}), NgoController.create)
 
-routes.get('/incidents', IncidentController.index)
-routes.post('/incidents', IncidentController.create)
-routes.delete('/incidents/:id', IncidentController.delete)
+routes.get('/profile', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required()
+  }).unknown()
+}), ProfileController.index)
+
+routes.get('/incidents', celebrate({
+  [Segments.QUERY]: Joi.object().keys({
+    page: Joi.number()
+  })
+}),IncidentController.index)
+
+routes.post('/incidents', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required()
+  }).unknown(),
+
+  [Segments.BODY]: {
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    value: Joi.number().required()
+  }
+}),IncidentController.create)
+
+routes.delete('/incidents/:id', celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.number().required()
+  })
+}),IncidentController.delete)
 
 // Exportando a variável para disponibilizá-la:
 module.exports = routes
